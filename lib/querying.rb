da8e8cb93dcc3b8219e947e4cfbcab89327ecabe
Oscar Shoudt <shoudto@gmail.com>
2019-08-29 20:04:23 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  ""
+  "SELECT characters.species, COUNT(characters.species)
+   FROM characters
+   WHERE species = ''"
 end
 
 def select_name_and_series_subgenres_of_authors

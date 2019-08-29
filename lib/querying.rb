7d9fd1bedb1cd0ea83fe6fd632c0f3c2daf2d26a
Oscar Shoudt <shoudto@gmail.com>
2019-08-29 19:56:37 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -10,7 +10,8 @@ end
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto
    FROM characters
-   "
+   GROUP BY characters.motto
+   LIMIT 1;"
 end
 
 
